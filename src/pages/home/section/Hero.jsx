a1a6afbeb215dedcf865ad72fa079652a19a5042
zhafrana <mz.arman@gmail.com>
2023-10-11 09:12:52 +0700

    Test Header

--- src/pages/home/section/Hero.jsx
+++ src/pages/home/section/Hero.jsx
@@ -1,59 +1,37 @@
-import illu_1 from "../../../assets/img/illu_header_1.png"
-import illu_2 from "../../../assets/img/illu_header_2.png"
-import illu_3 from "../../../assets/img/illu_header_3.png"
+import illu from "../../../assets/img/illu.png"
 import { motion as m } from 'framer-motion'
 
 
 const Hero = () => {
 
-  const textRow1 = "We Are digitalkultr"
-  const textRow2 = "a digital agency based in Bandung."
-  const textRow3 = "We craft digital experience"
-  const textRow4 = "for the people we care & love."
+  const textRow1 = "We're digitalkultr, a digital agency crafting digital experiences for the ones we cherish."
 
   const wordAni = {
-    hidden: { y: "100%"},
+    hidden: { y: "100%" },
     visible: (i) => ({
       y: "0%",
-      transition: {duration: 0.5, delay: 0.1 * i}
+      transition: { duration: 0.4, delay: 0.09 * i }
     }),
   }
-  
+
 
   return (
     <section className="hero-section container">
-      <h1 className='hero-text'>
-        <div className="hero-text-row">
+      <div className='hero-text-container'>
+        <h1 data-srcroll data-scroll-speed="0.3" className="hero-text">
           {
             textRow1.split(" ").map((word, index) => {
-              return <span key={index} className="word-mask"><m.span className="word" variants={wordAni} initial="hidden" animate="visible" custom={index}>{word}</m.span></span>
-            })
-          } <img className="illu-header" src={illu_1} />
-        </div>
-        <div className="hero-text-row">
-          {
-            textRow2.split(" ").map((word, index) => {
-              return <span key={index} className="word-mask"><m.span className="word" variants={wordAni} initial="hidden" animate="visible" custom={index}>{word}</m.span></span>
+              return <div key={index} className="word-mask"><m.span className="word" variants={wordAni} initial="hidden" animate="visible" custom={index}>{word}&nbsp;</m.span></div>
             })
           }
-        </div>
-        <div className="hero-text-row">
-          {
-            textRow3.split(" ").map((word, index) => {
-              return <span key={index} className="word-mask"><m.span className="word" variants={wordAni} initial="hidden" animate="visible" custom={index}>{word}</m.span></span>
-            })
-          }
-          <img className="illu-header" src={illu_2} />
-        </div>
-        <div className="hero-text-row">
-          <img className="illu-header" src={illu_3} />
-          {
-            textRow4.split(" ").map((word, index) => {
-              return <span key={index} className="word-mask"><m.span className="word" variants={wordAni} initial="hidden" animate="visible" custom={index}>{word}</m.span></span>
-            })
-          }
-        </div>
-      </h1>
+        </h1>
+      </div>
+      <div className="illu-container">
+        <img data-scroll data-scroll-speed="0.1" src={illu} className="illu" />
+      </div>
+      <div className="cta-container" >
+        <a data-scroll data-scroll-speed="0.2" className="cta-header">Explore playground</a>
+      </div>
     </section>
   );
 }
